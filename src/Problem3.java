08428de432f44f66580981388a209c55daebb422
aptmac <aptmac@yorku.ca>
2016-02-17 21:19:22 -0800

    Problem 3
    Solution to Problem 3

--- src/Problem3.java
+++ src/Problem3.java
@@ -5,38 +5,29 @@ import java.util.ArrayList;
  * Date: February 17th, 2016
  * Description: The prime factors of 13195 are 5, 7, 13, and 29
  * 				What is the largest prime factor of 600851475143?
+ * Idea: Continuously divide the number by incrementing values until
+ * 		 we arrive at the largest prime factor
  */
 
 public class Problem3 {
 
 	public static void main(String[] args) {
 		
+		// Define variable to hold the number of interest
 		long num = 600851475143L;
-		long half = num/2;
 		
-		ArrayList<Long> primeNumbers = new ArrayList<Long>();
-		primeNumbers.add(2L);
+		// Print the result to console
+		System.out.println(getLargestFactor(num));
+	}
+	
+	public static long getLargestFactor(long num) {
 		
-		for (long i = 2; i < half; i++)
-		{
-			Boolean isPrime = true;
-			
-			for (Long p : primeNumbers)
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0)
 			{
-				if (i % p == 0 || num % p == 0)
-				{
-					isPrime = false;
-					break;
-				}
-			}
-			
-			if (isPrime == true)
-			{
-				primeNumbers.add(i);
-				System.out.println("Prime: " + i);
+				num = num/i;
 			}
 		}
-		System.out.print("The largest prime factor of " + num + " is: ");
-		System.out.println(primeNumbers.get(primeNumbers.size()-1));
+		return num;
 	}
 }
