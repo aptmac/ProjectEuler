58cbb7ad526a29664ab6195be4d0655d86e49ea1
aptmac <apmacdon@sfu.ca>
2016-02-23 22:40:05 -0800

    Problem 9 - Work in Progress

---
+++ src/Problem9.java
@@ -0,0 +1,17 @@
+/**
+ * Project Euler Problem #9: Special Pythagorean triplet
+ * Date: February 22nd, 2016
+ * Description: A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * 				a2 + b2 = c2; For example, 32 + 42 = 9 + 16 = 25 = 52.
+ * 				There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ *				Find the product abc.
+ */
+
+public class Problem9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
